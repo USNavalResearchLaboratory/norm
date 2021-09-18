12c729aaffc20b11d3892c521123d98c6724e21a
bebopagogo <bebopagogo@users.noreply.github.com>
2021-09-17 20:16:41 -0400

    added Android gradle CMakelists file, etc

---
+++ android/lib/.externalNativeBuild/cmake/release_Release/x86/CMakeFiles/3.6.0-rc2/CMakeASMCompiler.cmake
@@ -0,0 +1,12 @@
+set(CMAKE_ASM_COMPILER "/Users/adamson/Library/Android/sdk/ndk-bundle/toolchains/llvm/prebuilt/darwin-x86_64/bin/clang")
+set(CMAKE_ASM_COMPILER_ARG1 "")
+set(CMAKE_AR "/Users/adamson/Library/Android/sdk/ndk-bundle/toolchains/x86-4.9/prebuilt/darwin-x86_64/bin/i686-linux-android-ar")
+set(CMAKE_RANLIB "/Users/adamson/Library/Android/sdk/ndk-bundle/toolchains/x86-4.9/prebuilt/darwin-x86_64/bin/i686-linux-android-ranlib")
+set(CMAKE_LINKER "/Users/adamson/Library/Android/sdk/ndk-bundle/toolchains/x86-4.9/prebuilt/darwin-x86_64/bin/i686-linux-android-ld")
+set(CMAKE_ASM_COMPILER_LOADED 1)
+set(CMAKE_ASM_COMPILER_ID "Clang")
+set(CMAKE_ASM_COMPILER_ENV_VAR "ASM")
+
+set(CMAKE_ASM_IGNORE_EXTENSIONS h;H;o;O;obj;OBJ;def;DEF;rc;RC)
+set(CMAKE_ASM_LINKER_PREFERENCE 0)
+
